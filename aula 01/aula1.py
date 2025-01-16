743e44fb9d4ad2907364ab2cdf0ac7ab8561da90
Antonio José dos santos menezes <antoniojoseyt341@gmail.com>
2025-01-16 12:19:38 -0300

    primeiro commit aula de tipos de dados

---
+++ aula 01/aula1.py
@@ -0,0 +1,9 @@
+nome = 'antonio' #String 
+idade = '16 anos' #int
+altura=  1.78 #float
+gosto_de = 'programar' 
+
+print(f'meu nome e {nome}')
+print(f'minha idade e {idade}')
+print(f'minha altura e {altura}')
+print(f'eu gosto de {gosto_de}')
