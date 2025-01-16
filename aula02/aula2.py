8808c43ce836657e64290c916cbeba47eb002e68
Antonio José dos santos menezes <antoniojoseyt341@gmail.com>
2025-01-16 12:58:14 -0300

    2 commit aula 2

--- aula02/aula2.py
+++ aula02/aula2.py
@@ -13,4 +13,25 @@ b=int(num2)
 print(b)
 print(type(b))
 
+#operadores
+#print (num1 +  num2) #soma 
+#print (num1 -  num2) #subitração
+#print (num1 *  num2) #multiplicação
+#print (num1 /  num2) #divisão
+#print (num1 %  num2) #resto da divisão
+#print (num1 ** num2) #exponenciação
+#print (num1 // num2) #retorna inteiros da divisão
+
+print(abs(-15)) #absoluto
+print(pow(3,3)) #exponenciação
+print(max(1,5)) #maior numero
+print(min(1,5)) #menor numero
+print(round(8.8)) #arredondamento
+
+#biblioteca de matematica
+import math
+
+print(math.floor(8.9999))#arredonda para baixo
+print(math.ceil(8.00000001))#arredonda para cima
+print(math.sqrt(8.0000000000))# raiz quadrada
 
