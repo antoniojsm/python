485b343333f903c8f1cabca7a5d78711f9dafee0
Antonio José dos santos menezes <antoniojoseyt341@gmail.com>
2025-01-17 16:40:33 -0300

    primeiro commit aula 5

---
+++ aula 05/aula5.py
@@ -0,0 +1,23 @@
+# collections, lists e tuples
+
+familia=['antonio','pedro','maria','ricardo']
+        #    0        1       2        3
+print(familia) #imprimir a lista 
+print(familia[0]) #primeira pessoa da lista
+print(familia[1:3])#retorna as pessoas apartir de 1, excluindo o 3
+
+familia[1]='matheus' #troca o nome de pedro para matheus
+
+familia.extends(['francisco','clara']) #adiciona mais 2 nomes a familia
+
+familia.append('jupi') #adiciona apenas um valor a lista
+
+familia.insert(1,'jupi')#ele vira o valor numero 1
+
+familia.remove('jupi') #remove o nome jupi
+
+idade_familia=[34,24,13,11,4]
+print(idade_familia)
+idade_familia.sort()#ordenar em ordem crescente
+print(idade_familia)
+
