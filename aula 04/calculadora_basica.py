6dfefa4bf8815a6665efe9f294265c88048d5900
Antonio José dos santos menezes <antoniojoseyt341@gmail.com>
2025-01-17 16:35:58 -0300

    segundo commit calculadora basica

--- aula 04/calculadora_basica.py
+++ aula 04/calculadora_basica.py
@@ -1,7 +1,7 @@
 #calculadora basica
 
-num1=int(input('digite um numero:'))
-num2=int(input('digite outro numero:'))
+num1=float(input('digite um numero:'))
+num2=float(input('digite outro numero:'))
 
 print ('digite 1 para adição')
 print ('digite 2 para subtração')
@@ -11,13 +11,17 @@ print ('digite 4 para divisão')
 opcoes=input('digite qual calculo devo realizar:')
 
 if (opcoes=='1'):
-    print(num1+num2)
+    resultado = num1 + num2
+    print(f"O resultado de {num1} + {num2} é {resultado}")
 elif opcoes=='2':
-    print(num1-num2)
+    resultado = num1 - num2
+    print(f"O resultado de {num1} - {num2} é {resultado}")
 elif opcoes=='3':
-    print(num1*num2)
+    resultado = num1 * num2
+    print(f"O resultado de {num1} * {num2} é {resultado}")
 else:
-    print(num1/num2)
+    resultado = num1 / num2
+    print(f"O resultado de {num1} / {num2} é {resultado}")
 
 
 
