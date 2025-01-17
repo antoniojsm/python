89f5ff15439513d1b3804b77109d38e1547b4ad8
Antonio José dos santos menezes <antoniojoseyt341@gmail.com>
2025-01-17 11:38:30 -0300

    primeiro commit aula de strings

---
+++ aula 03/aula3.py
@@ -0,0 +1,15 @@
+# Aula sobre strings
+
+minha_string='qualquer texto'
+             #01234567891111
+                       #0123
+print(minha_string.upper()) #tudo maiusculo
+print(minha_string.lower()) #tudo minusculo
+print(minha_string.capitalize()) #primeira letra maiuscula
+print(minha_string.isupper())#retorna verdadeiro ou falso se o texto e maiusculo
+print(minha_string.islower())# verifica se e minusculo
+print(minha_string.strip())# remove espaços no texto
+print(minha_string.replace('qualquer','meu'))#muda uma palavra por outra
+print(len(minha_string))#conta quantas letras tem a string
+print(minha_string[0]) #pega somente uma letra
+
